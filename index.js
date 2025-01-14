bf057f4402a6ecae773da5f147a5175588318ab1
hanifraihan123 <hanifraihan561@gmail.com>
2025-01-15 02:35:00 +0600

    users collection api done

--- index.js
+++ index.js
@@ -27,9 +27,21 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const userCollection = client.db('micro-task').collection('users')
+
+    app.post('/users', async(req,res)=>{
+      const user = req.body;
+      const query = {email: user.email}
+      const alreadyExist = await userCollection.findOne(query)
+      if(alreadyExist){return res.status(400).send('Already Exist')}
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
